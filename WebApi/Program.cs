bdc2599ea3f091ca3e99398f109467b835133d91
Valdir Ferreira Gomes da Silva <valdir.silva@fcamara.com.br>
2023-05-01 10:33:21 -0300

    Inclusão de Configurações de Cors

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -87,6 +87,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+var devClient = "http://localhost:4200";
+
+app.UseCors(x =>
+x.AllowAnyOrigin()
+.AllowAnyMethod()
+.AllowAnyHeader()
+.WithOrigins(devClient));
+
+
 app.UseHttpsRedirection();
 
 //New
